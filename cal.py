c1281432b441f6f9611f4d922884163a5f8e75f3
Tony <tonyvar666@gmail.com>
2024-04-21 18:01:51 +0530

    Calculator function commented out and a new function to print a pattern of stars added
    `cal.py`:
    - Calculator function with operator overloading for addition, subtraction and multiplication commented out
    - New function

--- cal.py
+++ cal.py
@@ -1,24 +1,27 @@
-def calculator(expression):
-    operators = {
-        '+': lambda x, y: x + y,
-        '-': lambda x, y: x - y,
-        '*': lambda x, y: x * y,
-    }
-    result = 0
-    current_operator = '+'
-    current_number = 0
+# def calculator(expression):
+#     operators = {
+#         '+': lambda x, y: x + y,
+#         '-': lambda x, y: x - y,
+#         '*': lambda x, y: x * y,
+#     }
+#     result = 0
+#     current_operator = '+'
+#     current_number = 0
 
-    for char in expression:
-        if char.isdigit():
-            current_number = current_number * 10 + int(char)
-        elif char in operators:
-            result = operators[current_operator](result, current_number)
-            current_number = 0
-            current_operator = char
+#     for char in expression:
+#         if char.isdigit():
+#             current_number = current_number * 10 + int(char)
+#         elif char in operators:
+#             result = operators[current_operator](result, current_number)
+#             current_number = 0
+#             current_operator = char
 
-    result = operators[current_operator](result, current_number)
+#     result = operators[current_operator](result, current_number)
 
-    return result
+#     return result
 
-expression = input('enter the value: ')
-print('the result= ', calculator(expression))
+# expression = input('enter the value: ')
+# print('the result= ', calculator(expression))
+arr = [f"{i}*2={i*2}" for i in range(1,26)]
+for i in arr:
+    print(i)
--- patter1.py
+++ patter1.py
@@ -1,5 +1,5 @@
 def pattern(n):
-    for i in range(n+1,-1,-1):
+    for i in range(0,n+1):
         print(' '*(n-i),f'*'*(2*i+1))
         # print(str(i) *i)
 
