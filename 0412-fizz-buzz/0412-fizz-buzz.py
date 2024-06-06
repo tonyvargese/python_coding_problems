006557b5895ad4b362bb4aa1673d87a0189c6032
Tony Varghese <135459489+tonyvargese@users.noreply.github.com>
2024-06-06 21:08:57 +0530

    Time: 44 ms (48.08%), Space: 17.5 MB (92.59%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        stack=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                stack.append("FizzBuzz")
+            elif i%3==0:
+                stack.append("Fizz")
+            elif i%5==0:
+                stack.append("Buzz")
+            else:
+                stack.append(str(i))
+        return stack
+
+
+
+
+        
